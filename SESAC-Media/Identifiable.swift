6e47cdfcec06ac39cfe64bda1137545a57e2eac3
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2024-01-30 20:43:44 +0900

    SESAC 26회차 과제 : TableView 구현

---
+++ SESAC-Media/Identifiable.swift
@@ -0,0 +1,8 @@
+//
+//  Identifiable.swift
+//  SESAC-Media
+//
+//  Created by Seryun Chun on 2024/01/30.
+//
+
+import Foundation
---
+++ SESAC-Media/TvTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TvTableViewCell.swift
+//  SESAC-Media
+//
+//  Created by Seryun Chun on 2024/01/30.
+//
+
+import UIKit
+
+class TvTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
